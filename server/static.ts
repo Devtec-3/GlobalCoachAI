ba158ed3275595821e708155b8b6965ee72059f4
Muhammad Abdulwadud Ayinde <muhammabdulwadudalata@gmail.com>
2025-12-19 11:01:30 +0100

    Fix __dirname for ESM on Render

--- server/static.ts
+++ server/static.ts
@@ -1,12 +1,30 @@
 import express, { type Express } from "express";
 import fs from "fs";
 import path from "path";
+import { fileURLToPath } from "url";
+
+// Recreate __dirname for ES Modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export function serveStatic(app: Express) {
-  const distPath = path.resolve(__dirname, "public");
+  // We go up one level (..) because on Render, the 'public' folder
+  // is usually sitting next to the 'dist' folder or inside it
+  const distPath = path.resolve(__dirname, "..", "public");
+
   if (!fs.existsSync(distPath)) {
+    // If the first check fails, try looking inside 'dist' (common for some build tools)
+    const fallbackPath = path.resolve(__dirname, "public");
+    if (fs.existsSync(fallbackPath)) {
+      app.use(express.static(fallbackPath));
+      app.use("*", (_req, res) => {
+        res.sendFile(path.resolve(fallbackPath, "index.html"));
+      });
+      return;
+    }
+
     throw new Error(
-      `Could not find the build directory: ${distPath}, make sure to build the client first`,
+      `Could not find the build directory: ${distPath}, make sure to build the client first`
     );
   }
 
